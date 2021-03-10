84fe9b2432570d896b685556ee1d6c8775197c02
Wizard Hat <Nlarsen23.student@gmail.com>
2021-03-10 02:15:28 +0000

    Depersonalized message logic

--- hotline/logic.py
+++ hotline/logic.py
@@ -5,11 +5,11 @@ def message_logic(COOLDOWNS):
     messages = sum([COOLDOWNS[x]['use-count'] for x in COOLDOWNS])
 
     if messages < 1:
-        run_script('dog_light_off')
+        run_script('light_off')
     elif messages == 1:
-        run_script('dog_light_green')
+        run_script('light_green')
     elif 1 < messages < 10:
-        run_script('dog_light_yellow')
+        run_script('light_yellow')
     else:
-        run_script('dog_light_red')
+        run_script('light_red')
         
